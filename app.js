3cdc5c814a79c1682d62edb5be006fed319d57d9
sebastian tobito saavedra <sebastiantobito@gmail.com>
2024-01-22 14:56:44 -0500

    subiendo el ejercicio con condicional y comentarios

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+//Declaramos variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un n�mero por favor:");
+let numeroDeUsuario = prompt("Dime un número  entre 1 y 1 por favor:");
 
-console.log(numeroUsuario);
+//Mostramos el numero en consola
+console.log(numeroDeUsuario);
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el n�mero');
+//se hace la comparacion
+if (numeroDeUsuario == numeroSecreto) {
+    //La respuesta en caso de que se cumpla la condición
+    alert(" ¡Acertaste el numero!");
+}else{
+     //La respuesta en caso de que no se cumpla la condición
+    alert("No acertaste");
 }
 
