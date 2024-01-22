3a69492deb5dda89416e4d2559a2a1405acab5e7
sebastian tobito saavedra <sebastiantobito@gmail.com>
2024-01-22 15:02:19 -0500

    aplicando template strings codigo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Declaramos variables
 let numeroSecreto = 6;
-let numeroDeUsuario = prompt("Dime un número  entre 1 y 1 por favor:");
+let numeroDeUsuario = prompt("Dime un número  entre 1 y 10 por favor:");
 
 //Mostramos el numero en consola
 console.log(numeroDeUsuario);
@@ -8,7 +8,7 @@ console.log(numeroDeUsuario);
 //se hace la comparacion
 if (numeroDeUsuario == numeroSecreto) {
     //La respuesta en caso de que se cumpla la condición
-    alert(" ¡Acertaste el numero!");
+    alert(`¡Acertaste!,  el numero es: ${numeroSecreto}`);
 }else{
      //La respuesta en caso de que no se cumpla la condición
     alert("No acertaste");
