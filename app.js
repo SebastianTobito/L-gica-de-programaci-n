2d2d805d1979ed25167187f26f2c3252abf34976
sebastian tobito saavedra <sebastiantobito@gmail.com>
2024-01-23 18:47:34 -0500

    arreglando codigo y agregando el condicional ternario

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ alert('Bienvenido al juego del número secreto');
 let numeroSecreto = 6;
 let numeroDeUsuario;
 let intentos = 1;
-let palabraIntentos = "intento";
+//let palabraIntentos = "intento";
+let maximosIntentos = 3;
 
 while(numeroDeUsuario != numeroSecreto){
     numeroDeUsuario = prompt("Dime un número  entre 1 y 10 por favor:");
@@ -20,7 +21,7 @@ while(numeroDeUsuario != numeroSecreto){
 
     if (numeroDeUsuario == numeroSecreto) {
         //La respuesta en caso de que se cumpla la condición
-        alert(`¡Acertaste!,  el numero es: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraIntentos}`);
+        alert(`¡Acertaste!,  el numero es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? "intento" : "intentos" }`);
         
     }else{
         if(numeroDeUsuario > numeroSecreto){
@@ -29,8 +30,14 @@ while(numeroDeUsuario != numeroSecreto){
             alert("el número secreto es mayor");
         }
         //incrementamos el contador de intentos cuando no se acierta
-       intentos = intentos + 1; 
-       palabraIntentos = "intentos";
+        //intentos = intentos + 1;
+        //intentos += 1;
+       intentos ++; 
+       //palabraIntentos = "intentos";
+       if (intentos > maximosIntentos) {
+        alert(`Llegaste al número máximo de intentos ${maximosIntentos}`);
+        break;
+       }
 
         //La respuesta en caso de que no se cumpla la condición
         //alert("No acertaste");
